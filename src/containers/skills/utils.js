dd9b8c802de8f469773dfa36b993b0f50331733c
khann.sovannaphom <khann.sovannaphom@gmail.com>
2024-10-15 20:05:06 +0700

    wip skills

---
+++ src/containers/skills/utils.js
@@ -0,0 +1,57 @@
+export const skillsData =[
+    {
+        labe: "FRONT END",
+        data: [
+            {
+                skillName: "HTML",
+                percentage: "90",
+            },
+            {
+                skillName: "CSS",
+                percentage: "60",
+            },
+            {
+                skillName: "JAVASRIPT",
+                percentage: "70",
+            },
+            {
+                skillName: "REACT JS",
+                percentage: "65",
+            },
+        ]
+    },
+    {
+        labe: "BACK END",
+        data: [
+            {
+                skillName: "JAVA",
+                percentage: "50",
+            },
+            {
+                skillName: "PYTHON",
+                percentage: "40",
+            },
+            {
+                skillName: "KOTLIN",
+                percentage: "50",
+            },
+            ]
+    },
+    {
+        labe: "DATABASE",
+        data: [
+            {
+                skillName: "MYSQL",
+                percentage: "40",
+            },
+            {
+                skillName: "ORACLE",
+                percentage: "20",
+            },
+            {
+                skillName: "PG ADMIN4",
+                percentage: "20",
+            },
+        ]
+    },
+]
