6e71912860d114711dd0f4832f445ede0930007d
KHANN Sovannaphom <sovannaphom.khann@sbilhbank.com.kh>
2024-10-16 08:35:53 +0700

    mm

--- src/containers/skills/indext.jsx
+++ src/containers/skills/indext.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BsInfoCircleFill } from "react-icons/bs";
 import PageHeaderContent from "../../components/pagHeaderContent";
+import { skillsData } from "./utils"
+import {Animate, AnimateKeyframes} from "react-simple-animate"
 
 const Skills = () => {
   return (
@@ -9,6 +11,44 @@ const Skills = () => {
         headerText="My Skills"
         icon={<BsInfoCircleFill size={40} />}
       />
+
+      <div className="skill__content-wrapper">
+        {
+          skillsData.map((item,i)=> (
+            <div key={i} className="skill__content-wrapper__inner-content">
+              <Animate
+              play
+              duration={1}
+              delay={0.3}
+              start={{transform: 'translateX(-200px)'}}
+              end={{transform: 'translateX(0px)'}}
+              >
+                <h3>{item.label}</h3>
+                <div>
+                  {item.data.map((skillItem, j)=>(
+                    <AnimateKeyframes
+                    play
+                    duration={1}
+                    keyframes={["oppcity: 1", "opacity: 0 "]}
+                    iterationCount="1"
+                    >
+                    <div className="progressBar-wrapper" key={j}>
+                      <p>{skillItem.skillName}</p>
+                      <line
+                      percen
+                      />
+
+                    </div>
+                    </AnimateKeyframes>
+                  ))}
+                </div>
+
+              </Animate>
+            </div>
+          ))
+        }
+      </div>
+
     </section>
   );
 };
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,60 @@
+export const skillsData = [
+  {
+    label: "FRONT END",
+    data: [
+      {
+        skillName: "HTML",
+        percentage: "90",
+      },
+      {
+        skillName: "CSS",
+        percentage: "60",
+      },
+      {
+        skillName: "RRACT JS",
+        percentage: "70",
+      },
+      {
+        skillName: "JAVASCRIPT",
+        percentage: "70",
+      },
+    ],
+  },
+
+  {
+    label: "BACK END",
+    data: [
+      {
+        skillName: "JAVA",
+        percentage: "70",
+      },
+      {
+        skillName: "PHYTHON",
+        percentage: "50",
+      },
+      {
+        skillName: "KOTLIN",
+        percentage: "60",
+      },
+    ],
+  },
+
+  {
+    
+    label: "DATABASES",
+    data: [
+      {
+        skillName: "JAVA",
+        percentage: "70",
+      },
+      {
+        skillName: "PHYTHON",
+        percentage: "50",
+      },
+      {
+        skillName: "KOTLIN",
+        percentage: "60",
+      },
+    ],
+  }
+];
