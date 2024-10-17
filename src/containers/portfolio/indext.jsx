a5e980c49869858c9e63b3becc3de951a690f0e8
KHANN Sovannaphom <sovannaphom.khann@sbilhbank.com.kh>
2024-10-17 17:14:35 +0700

    portfolio

--- src/containers/portfolio/indext.jsx
+++ src/containers/portfolio/indext.jsx
@@ -1,6 +1,31 @@
 import React from "react";
 import { BsInfoCircleFill } from "react-icons/bs";
 import PageHeaderContent from "../../components/pagHeaderContent";
+import ImageOne from "../../images/imag1.png";
+import './style.scss'
+
+const protfolioData = [
+  {
+    id: 2,
+    name: "Ecomerce",
+    image: ImageOne,
+  },
+];
+
+const filterData = [
+  {
+    filterId: 1,
+    label: "All",
+  },
+  {
+    filterId: 2,
+    label: "Development",
+  },
+  {
+    filterId: 3,
+    label: "Design",
+  },
+];
 
 const Portfolio = () => {
   return (
@@ -9,6 +34,28 @@ const Portfolio = () => {
         headerText="My Portfolio"
         icon={<BsInfoCircleFill size={40} />}
       />
+      <div className="portfolio__content">
+        <ul className="portfolio__content__filter">
+          {filterData.map((item) => (
+            <li key={item.filterId}>{item.label}</li>
+          ))}
+        </ul>
+        <div className="portfolio__content__cards">
+          {
+            protfolioData.map((item)=>(
+              <div className="portfolio__content__cards__item" key={item.id}>
+
+                <div className="portfolio__content__cards__item__img-wrapper">
+                  <a>
+                    <img alt="dummy data" src={item.image} />
+                  </a>
+                </div>
+
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </section>
   );
 };
--- src/containers/resume/utils.js
+++ src/containers/resume/utils.js
@@ -25,5 +25,32 @@ export const data = {
             subTitle: "AAAAAAA",
             description: "AAAAAAA",
         }
-    ]
+    ],
+    education: [
+        {
+            title: "A",
+            subTitle: "AAAAAAA",
+            description: "AAAAAAA",
+        },
+        {
+            title: "B",
+            subTitle: "AAAAAAA",
+            description: "AAAAAAA",
+        },
+        {
+            title: "C",
+            subTitle: "AAAAAAA",
+            description: "AAAAAAA",
+        },
+        {
+            title: "D",
+            subTitle: "AAAAAAA",
+            description: "AAAAAAA",
+        },
+        {
+            title: "E",
+            subTitle: "AAAAAAA",
+            description: "AAAAAAA",
+        }
+    ],
 }
