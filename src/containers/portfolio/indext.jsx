09ff592ff0048558062124ed8cf321003687a35a
KHANN Sovannaphom <sovannaphom.khann@sbilhbank.com.kh>
2024-10-18 16:57:48 +0700

    wip contact

--- src/containers/portfolio/indext.jsx
+++ src/containers/portfolio/indext.jsx
@@ -1,14 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsInfoCircleFill } from "react-icons/bs";
 import PageHeaderContent from "../../components/pagHeaderContent";
 import ImageOne from "../../images/imag1.png";
-import './style.scss'
+import "./style.scss";
 
 const protfolioData = [
   {
     id: 2,
     name: "Ecomerce",
     image: ImageOne,
+    link: "",
+  },
+  {
+    id: 3,
+    name: "Ecomerce",
+    image: ImageOne,
+    link: "",
+  },
+  {
+    id: 3,
+    name: "Ecomerce",
+    image: ImageOne,
+    link: "",
+  },
+  {
+    id: 3,
+    name: "Ecomerce",
+    image: ImageOne,
+    link: "",
+  },
+  {
+    id: 3,
+    name: "Ecomerce",
+    image: ImageOne,
+    link: "",
+  },
+  {
+    id: 3,
+    name: "Ecomerce",
+    image: ImageOne,
+    link: "",
+  },
+  {
+    id: 3,
+    name: "Ecomerce",
+    image: ImageOne,
+    link: "",
+  },
+  {
+    id: 3,
+    name: "Ecomerce",
+    image: ImageOne,
+    link: "",
+  },
+  {
+    id: 3,
+    name: "Ecomerce",
+    image: ImageOne,
+    link: "",
+  },
+  {
+    id: 3,
+    name: "Ecomerce",
+    image: ImageOne,
+    link: "",
   },
 ];
 
@@ -28,6 +83,28 @@ const filterData = [
 ];
 
 const Portfolio = () => {
+  const [filterValue, setFilterValue] = useState(1);
+  const [hoveredValue, setHoveredValue] = useState(null);
+
+  function handleFilter(currentId) {
+    setFilterValue(currentId);
+  }
+
+  function handleHover(indext) {
+    setHoveredValue(indext);
+  }
+
+  console.log("=============");
+  console.log(handleHover);
+  console.log("=============");
+
+  const filterItems =
+    filterValue === 1
+      ? protfolioData
+      : protfolioData.filter((item) => item.id === filterValue);
+
+  console.log(filterItems);
+
   return (
     <section id="protfolio" className="portfolio">
       <PageHeaderContent
@@ -37,23 +114,38 @@ const Portfolio = () => {
       <div className="portfolio__content">
         <ul className="portfolio__content__filter">
           {filterData.map((item) => (
-            <li key={item.filterId}>{item.label}</li>
+            <li
+              className={item.filterId === filterValue ? "active" : ""}
+              onClick={() => handleFilter(item.filterId)}
+              key={item.filterId}
+            >
+              {item.label}
+            </li>
           ))}
         </ul>
         <div className="portfolio__content__cards">
-          {
-            protfolioData.map((item)=>(
-              <div className="portfolio__content__cards__item" key={item.id}>
-
-                <div className="portfolio__content__cards__item__img-wrapper">
-                  <a>
-                    <img alt="dummy data" src={item.image} />
-                  </a>
-                </div>
-
+          {filterItems.map((item, indext) => (
+            <div
+              className="portfolio__content__cards__item"
+              key={`cardItem${item.name.trim()}`}
+              onMouseEnter={() => handleHover(indext)}
+              onMouseLeave={() => handleHover(null)}
+            >
+              <div className="portfolio__content__cards__item__img-wrapper">
+                <a>
+                  <img alt="dummy data" src={item.image} />
+                </a>
               </div>
-            ))
-          }
+              <div className="overlay">
+                {indext === hoveredValue && (
+                  <div>
+                    <p>{item.name}</p>
+                    <button>Visit</button>
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
